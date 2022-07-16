7319e32895533540c8c70141e7199ac9d87534ce
Ganesh Pilli <neghani@gmail.com>
2022-07-16 07:45:48 +0530

    first commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const session = require("express-session");
 
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(flash());
 app.use(
