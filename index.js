ade9ba81955a7a34583629cce3b898ac63716dd5
Ganesh Pilli <neghani@gmail.com>
2022-07-16 07:49:27 +0530

    first commit

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const router = require("./application");
 const bodyParser = require("body-parser");
 const env = require("dotenv");
 const bookRouter = require("./application/book-routes");
-const db = require('./db/db')
+const db = require("./db/db");
 const flash = require("express-flash-messages");
 const session = require("express-session");
-
-
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,7 +16,10 @@ app.use(
     secret: "secret",
   })
 );
-env.config();
+if (process.env.NODE_ENV != "production") {
+  env.config();
+}
+
 db.bootstrap();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
